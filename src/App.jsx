3795f66a71d08eec18dd8fa9356ced0105231bb8
venkatkalyan18 <kalyankoppala23@gmail.com>
2024-12-20 19:10:26 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
 import Navbar from "./components/Navbar";
-<<<<<<< HEAD
 import { Routes, Route } from 'react-router-dom'; // Use Routes and Route
 import Drench from "./games/Drench";
-=======
 import PatternGenrator from "./games/PatternGenerator";
->>>>>>> 48931816a1090cafb6a3959950f1a95d539438ce
+
 
 export default function App() {
   return (
     <div>
       <Navbar />
-<<<<<<< HEAD
       <Routes> {/* Wrap the routes inside Routes component */}
         <Route path="/drench" element={<Drench />} />
+        <Route path="/Pattern-genrator" element={<PatternGenrator/>}/>
       </Routes>
-=======
->>>>>>> 48931816a1090cafb6a3959950f1a95d539438ce
     </div>
   );
 }
