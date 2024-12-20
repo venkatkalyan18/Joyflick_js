cdbf58e55cc94316805e785cd998b64c64fb4bec
Kishan Pal <kishanpalh6145@gmail.com>
2024-12-20 19:43:06 +0530

    added terraingenerator component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Navbar from "./components/Navbar";
-import { Routes, Route } from 'react-router-dom'; // Use Routes and Route
+import { Routes, Route } from "react-router-dom"; // Use Routes and Route
 import Drench from "./games/Drench";
 import PatternGenrator from "./games/PatternGenerator";
-
+import TerrainGenerator from "./games/TerrainGenerator";
 
 export default function App() {
   return (
     <div>
       <Navbar />
-      <Routes> {/* Wrap the routes inside Routes component */}
+      <Routes>
+        {" "}
+        {/* Wrap the routes inside Routes component */}
         <Route path="/drench" element={<Drench />} />
-        <Route path="/Pattern-genrator" element={<PatternGenrator/>}/>
+        <Route path="/Pattern-generator" element={<PatternGenrator />} />
+        <Route path="/terrain-generator" element={<TerrainGenerator />} />
       </Routes>
     </div>
   );
